661b0cb768f0f39a5f724b041b7ad2c619ceaa82
Jesus Naranjo <jesusnaranjo375@gmail.com>
2022-03-06 14:49:31 -0800

    more style changes in js

--- fancifymytext.js
+++ fancifymytext.js
@@ -14,6 +14,9 @@ function fancy(){
     document.getElementById("text").style.transform = "rotate(7deg)";
     document.getElementById("text").style.textShadow = "5px 5px 1px #ff0000, 10px 10px 1px #0000ff";
     document.getElementById("text").style.justifyContent = "space-around";
+    document.getElementById("text").style.fontWeight="bold";
+    document.getElementById("text").style.color="blue";
+    document.getElementById("text").style.textDecoration="underline"
     alert('Fancified text!');
 }
 
@@ -21,6 +24,9 @@ function boring(){
     document.getElementById("text").style.transform = "none";
     document.getElementById("text").style.textShadow = "none";
     document.getElementById("text").style.justifyContent = "flex-start";
+    document.getElementById("text").style.fontWeight="normal";
+    document.getElementById("text").style.color="black";
+    document.getElementById("text").style.textDecoration="none"
 
     alert('Undid fancied text!');
 }
