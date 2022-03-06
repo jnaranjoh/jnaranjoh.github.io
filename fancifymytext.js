8d8179813e0f2f37cc78c48b93fb22d4e46cbdaa
Jesus Naranjo <jesusnaranjo375@gmail.com>
2022-03-06 14:47:38 -0800

    style changes in js

--- fancifymytext.js
+++ fancifymytext.js
@@ -3,18 +3,26 @@ const btn = document.querySelector('button');
 btn.addEventListener('click', () =>{
     makeBig();
 })
+
 function makeBig(){
-    document.getElementById("text").style.fontSize = "24px";
+    document.getElementById("text").style.fontSize = "24pt";
     //alert('Hello World!');
-    alert('big');
+    alert('Text enlarged by 24pt!');
 }
 
 function fancy(){
-    alert('fancy');
+    document.getElementById("text").style.transform = "rotate(7deg)";
+    document.getElementById("text").style.textShadow = "5px 5px 1px #ff0000, 10px 10px 1px #0000ff";
+    document.getElementById("text").style.justifyContent = "space-around";
+    alert('Fancified text!');
 }
 
 function boring(){
-    alert('borings');
+    document.getElementById("text").style.transform = "none";
+    document.getElementById("text").style.textShadow = "none";
+    document.getElementById("text").style.justifyContent = "flex-start";
+
+    alert('Undid fancied text!');
 }
 
 
