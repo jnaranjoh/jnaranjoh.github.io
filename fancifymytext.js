8985cdd9462ed40200bd114e58f4ae8c3812d5cc
Jesus Naranjo <jesusnaranjo375@gmail.com>
2022-03-06 13:19:45 -0800

    alerts added under js

---
+++ fancifymytext.js
@@ -0,0 +1,16 @@
+function makeBig(){
+    alert('big');
+}
+
+function change(){
+    alert('change');
+}
+
+function unchange(){
+    alert('unchanged');
+}
+
+
+function moo(){
+    alert('mmooo');
+}
