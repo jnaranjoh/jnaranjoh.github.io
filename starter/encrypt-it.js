4307e7a596c49fbfc150edc6c8e6b731ac8a4197
Jesus Naranjo <jesusnaranjo375@gmail.com>
2022-03-21 15:54:19 -0700

    fix format again..

--- starter/encrypt-it.js
+++ starter/encrypt-it.js
@@ -30,18 +30,22 @@ console.log("Window loaded!");
   function radio12(){
     if(document.getElementById("radio12").checked){
       document.getElementById("result").style.fontSize = "12pt";
+      document.getElementById("input-text").style.fontSize = "12pt";
+
     }
   }
 
   function radio24(){
     if(document.getElementById("radio24").checked){
       document.getElementById("result").style.fontSize = "24pt";
+      document.getElementById("input-text").style.fontSize = "24pt";
     }
   }
 
   function caps(){
     if(document.getElementById("all-caps").checked){
       document.getElementById("result").style.textTransform = "uppercase";
+      document.getElementById("intput-text").style.textTransform = "uppercase";
     }
   }
   
