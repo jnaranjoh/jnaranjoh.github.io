01b02b8b5ab31abc1439070a9154cd039ac117c3
Jesus Naranjo <jesusnaranjo375@gmail.com>
2022-03-21 15:31:18 -0700

    format changes

--- starter/encrypt-it.js
+++ starter/encrypt-it.js
@@ -33,6 +33,7 @@ console.log("Window loaded!");
       document.getElementById("result").style.fontSize = "12pt";
     }
   }
+
   function radio24(){
     if(document.getElementById("radio24".checked)){
       document.getElementById("result").style.fontSize = "24pt";
@@ -61,26 +62,26 @@ console.log("Window loaded!");
   }
 
   /**
- * Returns an encrypted version of the given text, where
- * each letter is shifted alphabetically ahead by 1 letter,
- * and 'z' is shifted to 'a' (creating an alphabetical cycle).
- */
-function shiftCipher(text) {
-  text = text.toLowerCase();
-  let result = "";
-  for (let i = 0; i < text.length; i++) {
-    if (text[i] < 'a' || text[i] > 'z') {
-      result += text[i];
-    } else if (text[i] == 'z') {
-      result += 'a';
-    } else { // letter is between 'a' and 'y'
-      let letter = text.charCodeAt(i);
-      let resultLetter = String.fromCharCode(letter + 1);
-      result += resultLetter;
+   * Returns an encrypted version of the given text, where
+   * each letter is shifted alphabetically ahead by 1 letter,
+   * and 'z' is shifted to 'a' (creating an alphabetical cycle).
+   */
+  function shiftCipher(text) {
+    text = text.toLowerCase();
+    let result = "";
+    for (let i = 0; i < text.length; i++) {
+      if (text[i] < 'a' || text[i] > 'z') {
+        result += text[i];
+      } else if (text[i] == 'z') {
+        result += 'a';
+      } else { // letter is between 'a' and 'y'
+        let letter = text.charCodeAt(i);
+        let resultLetter = String.fromCharCode(letter + 1);
+        result += resultLetter;
+      }
     }
+    return result;
   }
-  return result;
-}
   // Add any other functions in this area (you should not implement your
   // entire program in the init function, for similar reasons that
   // you shouldn't write an entire Java program in the main method).
